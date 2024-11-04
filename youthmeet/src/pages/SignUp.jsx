85832db6cf47f2d0cb175b0ace49424d436775e4
LukeFromAfar <lukasz.gamvik@gmail.com>
2024-11-04 13:27:20 +0100

    something with the navbar was maybe done?

--- youthmeet/src/pages/SignUp.jsx
+++ youthmeet/src/pages/SignUp.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import axios from "axios";
 
 export default function SignUp() {
+    const [username, setUsername] = useState();
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [repeatPassword, setRepeatPassword] = useState();
@@ -12,6 +13,7 @@ export default function SignUp() {
         axios.post(
             `${process.env.REACT_APP_BACKEND_URL}/api/user`,
             {
+                username,
                 email,
                 password, 
                 repeatPassword
@@ -26,9 +28,10 @@ export default function SignUp() {
         <>
             <h1>Create User</h1>
             <form onSubmit={handleSubmit} action="">
-                <input type="email" placeholder="Email" onChange={(e) => {setEmail(e.target.value)}}></input>
-                <input type="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}></input>
-                <input type="password" placeholder="Repeat password" onChange={(e) => {setRepeatPassword(e.target.value)}}></input>
+                <input name="username" type="text" placeholder="Username" onChange={(e) => {setUsername(e.target.value)}}></input>
+                <input name="email" type="email" placeholder="Email" onChange={(e) => {setEmail(e.target.value)}}></input>
+                <input name="password" type="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}></input>
+                <input name="repeatPassword" type="password" placeholder="Repeat password" onChange={(e) => {setRepeatPassword(e.target.value)}}></input>
                 <button>Register User</button>
             </form>
         </>
