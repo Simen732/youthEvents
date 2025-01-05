bfb829968e2cd0bafe44abff98b993e393938fa6
LukeFromAfar <lukasz.gamvik@gmail.com>
2025-01-05 21:28:09 +0100

    added naming for backend of CreateEvent and OpenEvent, and made Cancel in create event do something

--- youthmeet/src/pages/CreateEvent.jsx
+++ youthmeet/src/pages/CreateEvent.jsx
@@ -57,7 +57,7 @@ export default function CreateEvent() {
 
     try {
       // Send data to backend
-      const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/event/createEvent`, formData, {
+      const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/events/createEvent`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         },
@@ -66,7 +66,6 @@ export default function CreateEvent() {
 
       if (response.status === 200) {
         alert('Event created successfully!');
-        navigate("/events")
       } else {
         alert('Failed to create event.');
         console.log(response);
@@ -167,7 +166,7 @@ export default function CreateEvent() {
         </div>
 
         <div className="flex justify-end space-x-4">
-          <button type="button" onClick={() => console.log("Cancel")} className="px-4 py-2 bg-gray-300 rounded-md hover:bg-gray-400">
+          <button type="button" onClick={() => navigate("/events")} className="px-4 py-2 bg-gray-300 rounded-md hover:bg-gray-400">
             Cancel
           </button>
           <button type="submit" className="px-4 py-2 bg-orange-500 text-white rounded-md hover:bg-orange-600">
